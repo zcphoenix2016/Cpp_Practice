ae83e55878f07c91da5286c27be8cf13c5b0e76a
zcphoenix2016 <13564240769@139.com>
2016-10-28 14:40:57 +0800

    two-layer inheritance

--- inheritance.cpp
+++ inheritance.cpp
@@ -3,12 +3,25 @@
 class CBase
 {
 public:
+    void print()
+    {
+        std::cout << "CBase::print()" << std::endl;
+    }
     int m_x = 0;
 };
 
 //class CDerived : CBase
 //-- private inherit by default
 class CDerived : public CBase
+{
+public:
+    void print()
+    {
+        std::cout << "CDerived::print()" << std::endl;
+    }
+};
+
+class CcDerived : public CDerived
 {
 };
 
@@ -21,10 +34,13 @@ struct SDerived : CBase
 int main()
 {
     CDerived cd;
-    cd.m_x;
+    std::cout << "CDerived::m_x: " << cd.m_x << std::endl;
 
     SDerived sd;
-    sd.m_x;
+    std::cout << "SDerived::m_x: " << sd.m_x << std::endl;
+
+    CcDerived cc;
+    cc.print();//CDerived::print() will be invoked here.
 
     return 0;
 }
